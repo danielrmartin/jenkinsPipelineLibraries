59ff43206ec025571b7ca8089051dddcf3895b34
danielrmartin <danielraymartin@gmail.com>
2017-04-28 09:46:31 -0500

    Add library

---
+++ src/com/acme/jenkins/pipelines/build.groovy
@@ -0,0 +1,11 @@
+package com.acme.jenkins.pipeline.build
+class Build{
+def Maven(pomLocation, mvnTarget, mvnOptions)
+{
+sh "echo call maven with these arguments $pomLocation $mvnTarget $mvnOptions"
+}
+def nodejs (arg1, arg2, agr3)
+{
+ sh "echo these are my variables  $arg1 $arg2 $arg3"
+}
+}
