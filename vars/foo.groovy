9f0d3fff4bb64acc6085866a7f917a34e354ae44
danielrmartin <danielraymartin@gmail.com>
2017-04-28 08:27:16 -0500

    Change to declarative

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,5 +1,5 @@
 def call(String name='human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-   sh" echo Hello $name."
+   steps {echo Hello $name}
 }
