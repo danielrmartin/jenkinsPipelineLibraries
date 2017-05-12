f0aa40fe4f167730f2fd70155b92c42b789a620d
danielrmartin <danielraymartin@gmail.com>
2017-05-12 15:16:33 -0500

    Fix stages

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,10 +2,10 @@ def call(String name) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
    lock(quantity: 1, resource: 'deployment') {
-      stage('Deploying to $name'){
+      stage('Deploying to '+ name){
         sh "echo deploy to $name"
 	}
-	stage('Test $name')
+	stage('Test '+name)
 	{
 	sh "echo Testing env $name"
 	sleep time:2,duration:'MINUTES'
