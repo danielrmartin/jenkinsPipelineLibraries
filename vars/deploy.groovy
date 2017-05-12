5397ad6bbb597b044618725944ee83e7e56a0e98
danielrmartin <danielraymartin@gmail.com>
2017-05-12 15:37:23 -0500

    Change lock

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,14 +1,14 @@
 def call(String name) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-   lock(quantity: 1, resource: 'deployment') {
+   lock(quantity: 1, resource: 'deployment'+name) {
       stage('Deploying to '+ name){
         sh "echo deploy to $name"
 	}
 	stage('Test '+name)
 	{
 	sh "echo Testing env $name"
-	sleep time:2,duration:'MINUTES'
+	sleep time: 2,duration: 'MINUTES'
 	}
 	}//end lock
 }
