c559fd0210cad3f698d46395fa45a9abce457fc8
danielrmartin <danielraymartin@gmail.com>
2017-05-12 15:05:33 -0500

    Add lock

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,4 +2,10 @@ def call(String name) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
    sh "echo deploy to $name"
+   lock(quantity: 1, resource: 'deployment') {
+	stage('post deployment test')
+	{
+	sh "echo Testing env $name"
+	}
+	}//end lock
 }
