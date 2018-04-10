a9c7e66efd8d07a9a36cf81e774bc9248b96cc0f
danielrmartin <danielraymartin@gmail.com>
2018-04-09 21:31:02 -0500

    Update validation.groovy

--- vars/validation.groovy
+++ vars/validation.groovy
@@ -1,8 +1,9 @@
-def call(String Config){
-echo "Checking config.txt"
-def config = readYaml file:'config.yml'
-Java_Version=config.tools.Java
-Maven_Version=config.tools.Maven
-Build_target=config.Builds
-  sh " echo $config.Builds"
+def call(String Config)
+{
+  echo "Checking config.txt"
+  def config = readYaml file:'config.yml'
+  Java_Version=config.tools.Java
+  Maven_Version=config.tools.Maven
+  Build_target=config.Builds
+  sh " echo $Builds_target"
 }
