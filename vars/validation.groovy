3f202785535085a970f26de22aeb244407ff052f
danielrmartin <danielraymartin@gmail.com>
2018-04-09 21:08:46 -0500

    Update validation.groovy

--- vars/validation.groovy
+++ vars/validation.groovy
@@ -1,6 +1,6 @@
-def call(Config String){
+def call(String Config){
 echo "Checking config.txt"
-def config = readYaml file:Name
+def config = readYaml file:Config
 Java_Version=config.tools.Java
 Maven_Version=config.tools.Maven
 Build_target=config.Builds
