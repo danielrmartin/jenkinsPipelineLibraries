fba9d5e1b732cc071bc7b99925806110f478ed87
danielrmartin <danielraymartin@gmail.com>
2018-06-18 11:59:50 -0500

    Update Build.groovy

--- src/com/acme/Build.groovy
+++ src/com/acme/Build.groovy
@@ -2,7 +2,6 @@ package com.acme;
 
 class Build implements Serializable {
   def steps
-  Utilities(steps) {this.steps = steps}
   def mvn(args) {
     steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
   }
